fc727d6125b27b2b2cc356122472306071e13808
Kasun Thiwanka <kasunnthiwanka@gmail.com>
2021-03-02 13:10:02 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect } from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+import Feed from "./Feed";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
+import { useSelector, useDispatch } from "react-redux";
+import { auth } from "./firebase";
+import { login, logout } from "./features/userSlice";
+import Widgets from "./Widgets";
+
+function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    auth.onAuthStateChanged((userAuth) => {
+      if (userAuth) {
+        dispatch(
+          login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.displayName,
+            photoUrl: userAuth.photoURL,
+          })
+        );
+      } else {
+        dispatch(logout());
+      }
+    });
+  }, []);
+  return (
+    <div className="app">
+      <Header />
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          <Widgets />
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HeaderOption.js
@@ -0,0 +1,15 @@
+import { Avatar } from "@material-ui/core";
+import React, { onClick }  from "react";
+import "./HeaderOption.css";
+
+function HeaderOption({avatar, Icon, title}) {
+  return (
+    <div onClick={onClick} className="headerOption">
+      {Icon && <Icon className="headerOption__icon" />}
+      {avatar &&  <Avatar className="headerOption__icon" src={avatar} />}
+      <h3 className="headerOption__title">{title}</h3>
+    </div>
+  );
+}
+
+export default HeaderOption;
